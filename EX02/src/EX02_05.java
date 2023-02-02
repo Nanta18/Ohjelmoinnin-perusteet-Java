ee5ea4579bbb40089dc23f798bc8dc4e23cf01ee
Nanta <joonatan.vainiomaki@protonmail.com>
2023-02-02 19:28:33 +0200

    Finished EX02_05

--- EX02/src/EX02_05.java
+++ EX02/src/EX02_05.java
@@ -8,16 +8,29 @@ public class EX02_05 {
     static void kokeileLukualue() {
         Scanner kyselija = new Scanner(System.in); 
 		System.out.println("Kerro lukualueen yläraja");
-        int ylaraja = kyselija.nextInt();
+        int ylaraja = kyselija.nextInt();                       //Kysytään yläraja
         System.out.println("Kerro lukualueen alaraja");
-        int alaraja = kyselija.nextInt();
+        int alaraja = kyselija.nextInt();                       //Kysytään alaraja
 
 
         while(true) {
-            System.out.println("Kerro joku luku");
-            int jokuLuku = kyselija.nextInt();
-            if(jokuLuku < ylaraja && jokuLuku > alaraja) {
-                System.out.println("toimii se");
+            System.out.println("Kerro joku luku");      
+            int jokuLuku = kyselija.nextInt();                  //kysytään joku luku
+            if(jokuLuku < ylaraja && jokuLuku > alaraja) {      //jos luku on rajojen välissä jatketaan eteenpäin
+                if(jokuLuku < ((ylaraja + alaraja) / 2)) {       //jos luku on lähempänä alarajaa jatketaan
+                    System.out.println("Anna kolme negatiivista numeroa");
+                    int luku1 = kyselija.nextInt();
+                    int luku2 = kyselija.nextInt();
+                    int luku3 = kyselija.nextInt();
+
+                    int summa = luku1 + luku2 + luku3;
+                    System.out.println("Lukujen summa on" + summa);  //Tulostetaan lukujen summa
+                    kyselija.close();
+                    break;
+                } else {
+                    kyselija.close();
+                    break;
+                } 
             }
         }
     }
