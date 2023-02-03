7162516d2567ef12ae8dc16eb0593a4118ad91b8
Joonatan Vainiomäki <joonatan.vainiomaki@protonmail.com>
2023-02-03 11:09:37 +0200

    Added comments

--- EX01/src/EX01_04.java
+++ EX01/src/EX01_04.java
@@ -6,27 +6,25 @@ import java.util.Scanner;
 	
 		public static void main(String[] args) {
 	
-			Scanner kayttajaNumero = new Scanner(System.in);
+			Scanner kayttajaNumero = new Scanner(System.in);				//Luo kyselijän
 			System.out.println("Anna numero");
-			int numero_01 = kayttajaNumero.nextInt();
+			int numero_01 = kayttajaNumero.nextInt();						//Kysyy ensimmäisen luvun
 			
 			System.out.println("Anna toinen numero");
-			int numero_02 = kayttajaNumero.nextInt();
+			int numero_02 = kayttajaNumero.nextInt();						//Kysyy toisen luvun
 			
-			System.out.println("Anna Kolmaski numero");
+			System.out.println("Anna Kolmaski numero");					//Kysyy kolmannen luvun
 			int numero_03 = kayttajaNumero.nextInt();
 			
-			final ArrayList<Integer> numerot = new ArrayList<Integer>();
-			numerot.add(numero_01);
+			final ArrayList<Integer> numerot = new ArrayList<Integer>();	//Luo Arraylistan
+			numerot.add(numero_01);											//Lisää numerot
 			numerot.add(numero_02);
 			numerot.add(numero_03);
-
-			System.out.println(numerot);
 			
-			Collections.sort( numerot );
+			Collections.sort( numerot );									// Lajittelee numerot
 
-			System.out.println(numerot);
-			kayttajaNumero.close();
+			System.out.println(numerot);									//Tulostaa lajitellun listan
+			kayttajaNumero.close();											//Sulkee kyselijän
 		}
 	
 	}
