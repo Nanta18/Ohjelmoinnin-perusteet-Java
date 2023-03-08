a96265d1cdf8af2f9eb6efd41e9517ec51c205cc
Joonatan Vainiomäki <joonatan.vainiomaki@protonmail.com>
2023-03-08 15:53:00 +0200

    Created EX05_01

---
+++ EX05_01.java
@@ -0,0 +1,8 @@
+public class EX05_01 {
+    public static void main(String[] args) throws InterruptedException {
+        laskeKirjaimet();
+    }
+    public static int laskeKirjaimet(String jono, char merkki) {
+        return 0;
+    }
+}
