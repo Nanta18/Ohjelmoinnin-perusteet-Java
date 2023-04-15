8b83e90bdbd8b6ce20c9fb1a0125a9e1977e89e1
Nanta <joonatan.vainiomaki@protonmail.com>
2023-04-15 14:39:14 +0300

    Finished EX02

---
+++ EX09/src/EX09_02.java
@@ -0,0 +1,11 @@
+public class EX09_02 {
+    public static void main(String[] args) {
+        int i = 1;
+        for (int j = 0; j < 10; j++) {
+            int doubled = i * 2;
+            System.out.println(i + " times two equals " + doubled);
+            i++;
+        }
+        System.out.println("End of program");
+    }
+}
